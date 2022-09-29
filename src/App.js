b2972c879355b2bc55dfb2f3a3c3c996c6d91cab
[Fedxion] <[fedetoranzo89@gmail.com]>
2022-09-29 14:35:03 +0200

    logo

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/navegation/navbar"
  import Test from "./components/navegation/test"
 
+
 function App() {
   return <div className="App">
      <Navbar />
--- src/components/navegation/navbar.jsx
+++ src/components/navegation/navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Logo from "../../assets/logo.jpeg";
 
 function Navbar(){
     return <div>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-black">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Sabor a Campo</a>
+    <a className="navbar-brand" href="#">
+    <img src={Logo} /> </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
