6481a3ccc34dfa86787404ed2f2fefad6193c17c
gabiigodoy5 <gabrielagodoy.web@gmail.com>
2022-09-29 10:44:26 -0300

    last changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import campo from "../img/campo.png"
+
+
 function Test(){
     return <div>
     <h1>La cdlr</h1>
@@ -10,7 +13,7 @@ function Test(){
                         <div className='col-md-3 col-sm-6'>
                             <h4>Sabor a Campo</h4>
                             <ul className='list-unstyled'>
-                                <li><img src="./img/campo.png.crdownload" alt="" /></li>
+                                <li><img src={campo} alt="" /></li>
                                 <li>Tucumán, Argentina</li>
                                 <li>Monteagudo 345</li>
                             </ul>
