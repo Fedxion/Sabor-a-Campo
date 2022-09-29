7bc4f434b62885954767c75c7bb00857de72eff4
gabiigodoy5 <gabrielagodoy.web@gmail.com>
2022-09-29 12:56:58 -0300

    footer incompleto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
     <div>
      <h1>ESTO ES SABOR A CAMPO</h1>
      <p>viva la berjena</p>
+
+     <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Footer.css";
+import "./img/111.jpg";
+
+const Footer=()=>{
+    return <div>
+    
+    
+    <footer className="pie">
+            <div className="grupo1">
+                                        
+                            <div className='box'>
+                                <h2>Sabor a Campo</h2>
+                                <ul>
+                                    <li><img src="./img/111.jpg" alt="" /></li>
+                                </ul>
+                            </div>
+
+                            <div className='box'>
+                                <h2>SABOR A CAMPO</h2>
+                               <div className="link">
+                                    <a href="#">Inicio</a>
+                                </div>
+                                <div className="link">
+                                    <a href="#">Quienes Somos</a>
+                                </div>
+                                <div className="link">
+                                    <a href="#">Contacto</a>
+                                </div>
+                            </div>
+            
+            
+                            <div className='box'>
+                                <h2>REDES SOCIALES</h2>
+                                
+                                <div className='red-social'>
+                                    <a className="icon" href="#"><i class="fab fa-facebook"></i></a>
+                                    <a className="icon" href="#"><i class="fab fa-twitter"></i></a>
+                                    <a className="icon" href="#"><i class="fab fa-instagram"></i></a>
+                                    <a className="icon" href="#"><i class="fab fa-youtube"></i></a>
+                                </div>
+                            </div>
+                    
+
+
+            </div>
+
+
+                    <div className='grupo3'>
+                        <p className='copy'>
+                             &copy;{new Date().getFullYear()} Copyright
+                        </p>
+                    </div>
+
+                
+            
+    </footer>
+     
+   </div>
+}
+
+export default Footer;
