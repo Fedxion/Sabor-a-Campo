47e4933e7e2dadfc64ee566faa3b9e34573fe429
gabiigodoy5 <gabrielagodoy.web@gmail.com>
2022-10-03 20:22:44 -0300

    first changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from "react";
-import Navbar from "./components/navegation/navbar"
+import Navbar from "./components/navegation/navbar";
 import Footer from "./components/Footer";
-
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import AboutUs from "./components/pages/AboutUs";
 
 function App() {
-  return <div className="App">
-     <Navbar />
-     <Footer />
+  return (
+  
+  <BrowserRouter className="App">
+    <div className="container mt-5">
+      <div className="btn-group">
+        <Link to="/" className="btn btn-dark">
+          Inicio
+        </Link>
+      </div>
     </div>
+
+    <Switch>
+
+
+
+      <Route path="/AboutUs" element={Navbar} {...Imagen} {...Footer} />
+      <AboutUs />
+    {/* </Route> */}
+
+
+  </Switch>
+  
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/pages/AboutUs.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from "./components/navegation/navbar";
+import Footer from "./components/Footer";
+import Imagen from "../components/img/Logo.jpeg";
+import _default from '@popperjs/core/lib/modifiers/eventListeners';
+
+
+
+const AboutUs =()=>{
+    return <div>
+        <Navbar/>
+        <div className="container">
+            <img src={Imagen} alt="" />
+            <h1>NUESTRA HISTORIA</h1>
+            <h4>Sabor a Campo nace en el año 2020 en San Miguel de Tucumán a raíz de un deseo de realización personal y beneficio lucrativo.
+                Esta marca ofrece una amplia variedad de productos y servicios enfocados en la experiencia del consumidor, entre ellos: 15 variedades de escabeches y verduras, catering salado, delivery y take away boxes, así como diferentes opciones para eventos sociales y corporativos.
+
+                En el 2021 decidimos evolucionar inaugurando nuestro primer local en el centro de la ciudad de San Miguel de Tucumán.
+
+                Sabor a Campo elabora todos sus productos frescos de manera artesanal, sin conservantes ni aditivos y priorizando la alta calidad de materias primas</h4>
+        </div>
+        <Footer/>
+    </div>
+
+}
+
+export default AboutUs;
+
+
