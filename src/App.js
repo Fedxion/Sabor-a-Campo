19cc34e825506a0f2d4fd3521fc866f581957164
[Fedxion] <[fedetoranzo89@gmail.com]>
2022-10-03 18:26:44 +0200

    footer and navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import Navbar from "./components/navegation/navbar"
-import Test from "./components/navegation/test"
-
 import Footer from "./components/Footer";
 
 
 function App() {
   return <div className="App">
      <Navbar />
+     <Footer />
     </div>
 }
 
