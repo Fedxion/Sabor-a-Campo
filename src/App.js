20e5f493014038c020c5f955bfedf8696a999564
[Fedxion] <[fedetoranzo89@gmail.com]>
2022-10-03 20:19:15 +0200

    add Contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/navegation/navbar"
 import Footer from "./components/Footer";
+import Contact from "./components/pages/contact";
 
 
 function App() {
   return <div className="App">
      <Navbar />
+     
      <Footer />
     </div>
 }
