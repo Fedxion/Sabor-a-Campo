36eda1c3932e34ffc60c0fa5b5fe64f34c1d4520
[Fedxion] <[fedetoranzo89@gmail.com]>
2022-10-03 17:23:15 +0200

    navbar almost ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Navbar from "./components/navegation/navbar"
- import Test from "./components/navegation/test"
+import Test from "./components/navegation/test"
 
 
 function App() {
   return <div className="App">
      <Navbar />
-     <Test />
     </div>
 }
 
--- src/components/navegation/navbar.jsx
+++ src/components/navegation/navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../../assets/logo.jpeg";
+import "./navbar.css"
 
 function Navbar(){
     return <div>
@@ -12,30 +13,19 @@ function Navbar(){
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+      <li className="nav-item">
+          <a className="nav-link" href="#">Inicio</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+          <a className="nav-link" href="#">Quienes Somos</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
+          <a className="nav-link" href="#">Contacto</a>
         </li>
       </ul>
       <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
+        <input className="form-control me-2" type="search" placeholder="Escribir aqui" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Buscar</button>
       </form>
     </div>
   </div>
