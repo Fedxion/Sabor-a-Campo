43450117c2a7a76289f8cfbe3fca4470b1b40f85
[Fedxion] <[fedetoranzo89@gmail.com]>
2022-10-03 17:43:07 +0200

    footer almost finishing

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import Footer from "./components/Footer";
 
 
 function App() {
-  return (
-    <div>
-     <h1>ESTO ES SABOR A CAMPO</h1>
-     <p>viva la berjena</p>
-
-     <Footer/>
+  return <div>
+    
+    <Footer />
     </div>
-  );
 }
 
 export default App;
