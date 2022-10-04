54745c041a81d89e1c72bae8abde63046dc5ba5e
[Fedxion] <[fedetoranzo89@gmail.com]>
2022-10-04 15:02:15 +0200

    testing about us

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Navbar from "./components/navegation/navbar"
 import Footer from "./components/Footer";
 import Contact from "./components/pages/contact";
+import Test from "./components/navegation/test"
 
 
 function App() {
   return <div className="App">
      <Navbar />
-     
+     <Test />
      <Footer />
     </div>
 }
