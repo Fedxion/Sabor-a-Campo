b392198e25d3ce8e491aefda634822e938ab1e9c
[Fedxion] <[fedetoranzo89@gmail.com]>
2022-10-06 18:26:55 +0200

    add images and cards in homepage

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,26 +1,139 @@
 import React from "react";
-// import Logo from "../assets/logo.jpeg"
-// import Conserva from "../assets/conserva.jpg"
+import Berenjena from "../assets/Berenjena.jpeg";
+import Conejo from "../assets/Conejo.jpeg";
+import Pickles from "../assets/Pickles.jpeg";
+import Pollo from "../assets/Pollo.jpeg";
+import Porotos from "../assets/Porotos.jpeg";
+import PorotosNegros from "../assets/PorotosNegros.jpeg";
+import VerdAsadas from "../assets/Verduras Asadas.jpeg";
+import Verduras from "../assets/Verduras.jpeg";
 
-const HomePage=()=>{
-    return (
-        <div>
-           {/* <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={Logo} class="d-block w-100" alt="..." />
+import "../pages/homepage.css";
+
+const HomePage = () => {
+  return (
+    <div>
+    <div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={Berenjena} class="img-fluid rounded-start" alt="berenjena" />
     </div>
-    <div class="carousel-item">
-      <img src={Conserva} class="d-block w-100" alt="..." />
-    </div>
-    <div class="carousel-item">
-      <img src={Logo} class="d-block w-100" alt="..." />
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Berenjena</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
     </div>
   </div>
-</div> */}
-    <h1>Aqui va el carousel o cards, chau</h1>
-        </div>
-    )
-}
+  <div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={Conejo} class="img-fluid rounded-start" alt="Conejo" />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Conejo</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={Pickles} class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={Pollo} class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={Porotos} class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={PorotosNegros} class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={VerdAsadas} class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={Verduras} class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+  );
+};
 
 export default HomePage;
