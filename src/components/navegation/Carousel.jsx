c2e5bd3eb3e4a3874f30aa2862739d13cd486ac2
gabiigodoy5 <gabrielagodoy.web@gmail.com>
2022-10-07 12:22:23 -0300

    fallido

--- src/components/navegation/Carousel.jsx
+++ src/components/navegation/Carousel.jsx
@@ -1,20 +1,19 @@
 import React from "react";
-import Logo from "../../assets/logo.jpeg";
-import Conserva from "../../assets/conserva.jpg";
+import Img1 from "../../assets/img1.png";
+
+import "./Carousel.css";
 
 const Carousel=()=>{
     return (
         <div>
            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={Logo} class="d-block w-100" alt="..." />
+    <div class="carousel-item active" id="margen">
+      <img src={Img1} class="d-block w-100" id="img-carousel" alt="..." />
     </div>
-    <div class="carousel-item">
-      <img src={Conserva} class="d-block w-100" alt="..." />
-    </div>
-    <div class="carousel-item">
-      <img src={Logo} class="d-block w-100" alt="..." />
+    
+    <div class="carousel-item" id="margen">
+      <img src={Img1} class="d-block w-100" id="img-carousel" alt="..." />
     </div>
   </div>
 </div>
