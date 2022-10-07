e3868105180c7a1988af184c3c1b7fc60141da22
gabiigodoy5 <gabrielagodoy.web@gmail.com>
2022-10-07 11:47:58 -0300

    three

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/navegation/navbar";
 import Footer from "../src/components/navegation/Footer";
-import Carousel from "./components/navegation/carousel";
+import Carousel from "./components/navegation/Carousel";
 
 
 
---
+++ src/components/navegation/Carousel.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Logo from "../../assets/logo.jpeg";
+import Conserva from "../../assets/conserva.jpg";
+
+const Carousel=()=>{
+    return (
+        <div>
+           <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={Logo} class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src={Conserva} class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src={Logo} class="d-block w-100" alt="..." />
+    </div>
+  </div>
+</div>
+        </div>
+    );
+}
+
+export default Carousel;
