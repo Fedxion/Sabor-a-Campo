2e2ad04d3c4ada5c918cab1fb076daf7b887ba9c
gabiigodoy5 <gabrielagodoy.web@gmail.com>
2022-10-07 11:14:11 -0300

    carousel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/navegation/navbar";
 import Footer from "../src/components/navegation/Footer";
+import Carousel from "./components/navegation/carousel";
 
 
 
@@ -11,6 +12,7 @@ import Footer from "../src/components/navegation/Footer";
 function App() {
   return <div className="App">
      <Navbar />
+     <Carousel />
      <Footer />
     </div>
 }
