8eeaee22be6b4212ac99605f53c42142e246a062
gabiigodoy5 <gabrielagodoy.web@gmail.com>
2022-10-09 15:25:39 -0300

    last

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,22 +1,22 @@
-// import React from "react";
-// import {Routes, Route, BrowserRouter as Router }from "react-router-dom";
-// import About from "../pages/AboutUs";
-// import Contact from "../pages/contact";
-// import HomePage from "../pages/homePage";
+import React from "react";
+import {Routes, Route, BrowserRouter as Router }from "react-router-dom";
+import About from "../pages/AboutUs";
+import Contact from "../pages/contact";
+import HomePage from "../pages/homePage";
 
-// export const AppRouter = () => {
-//     return(
-//         <Router>
-//             <Routes>
+export const AppRouter = () => {
+    return(
+        <Router>
+            <Routes>
 
-//                 <Route exact path="/" component={HomePage} />
-//                 <Route exact path="/AboutUs" component={About} />
-//                 <Route exact path="/Contact" component={Contact} />
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/AboutUs" component={About} />
+                <Route exact path="/Contact" component={Contact} />
                 
-//             </Routes>
+            </Routes>
 
-//         </Router>
-//     )
-// }
+        </Router>
+    )
+}
 
-// export default AppRouter;
+export default AppRouter;
