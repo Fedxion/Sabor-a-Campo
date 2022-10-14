1c3af924f7d96e650bab5cf50acddff4248e3cc0
gabiigodoy5 <gabrielagodoy.web@gmail.com>
2022-10-14 18:57:26 -0300

    string

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,38 @@
 const express = require("express");
+const path = require('path');
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 const bodyparser = require("body-parser");
-const LoginRoute = require("./route/LoginRoute");
+const bcrypt =require('bcrypt');
+const mongoose =require('mongoose');
+// const LoginRoute = require("./route/LoginRoute");
 
-app.use(cors());
-app.use(express.json());
-app.use(LoginRoute);
-app.use(bodyparser.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use(express.json());
+// app.use(LoginRoute);
 app.use(bodyparser.json());
-app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(_dirname, 'public')));
+
+const mongo_uri = 'mongodb://dev:dev@localhost/todos';
+
+mongoose.connect(mongo_uri, function(err){
+    if(err){
+        throw err;
+    }else{
+        console.log('Successfully connected to ${mongo_uri});
+    }
+});
+
+app.get('/', (req, res)=>{
+
+});
+
+// app.use(express.static("public"));
+
+app.listen(3000, ()=>{
+    console.log('server started');
+});
 
 module.exports = app;
--- Backend/src/model/User.js
+++ Backend/src/model/User.js
@@ -1,17 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt= require('bcrypt');
 
-const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      require: true,
-    },
-    password: {
-      type: String,
-      require: true,
-    },
-  },
-  { collection: "User" }
-);
-module.exports = mongoose.model("User", userSchema);
+const saltRounds = 10;
+
+const UserShema = new mongoose.Schema({
+  email: {type:String, required:true, unique: true},
+  password:{type:String, required:true}
+});
+
+// const userSchema = new Schema(
+//   {
+//     email: {
+//       type: String,
+//       require: true,
+//     },
+//     password: {
+//       type: String,
+//       require: true,
+//     },
+//   },
+//   { collection: "User" }
+// );
+// module.exports = mongoose.model("User", userSchema);
